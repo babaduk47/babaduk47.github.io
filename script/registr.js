14b2f87ab1d6ff0c9570c8ae65993edced713c62
babaduk47 <56674801+babaduk47@users.noreply.github.com>
2019-10-18 15:17:55 +0300

    Add files via upload

--- script/registr.js
+++ script/registr.js
@@ -1,6 +1,13 @@
 var login = document.getElementsByName("login")[0];
 var pass = document.getElementsByName("pass")[0];
 var pass2 = document.getElementsByName("pass2")[0];
+var about = document.getElementsByName("about")[0];
+var ArrayPole=[false,false,false,false];
+var LeftPX=[0,150,300,450,600];
+var Proc=[0,25,50,75,100];
+var p=0;
+var brr = document.getElementById("brick");
+var l=0;
 
 function onchangelogin(){
 	var text = login.value;
@@ -24,10 +31,12 @@ function onchangelogin(){
 	}
 	if(error!=""){
 		alert(error);
+		login.style.border="2px solid red";
 		ArrayPole[0]=false;
 		anim();
 	}
 	else{
+		login.style.border="2px solid #49a35d";
 		ArrayPole[0]=true;
 		anim();
 	}
@@ -42,18 +51,35 @@ function onchangepass(){
 	}
 	if(error!=""){
 		alert(error);
+		pass.style.border="2px solid red";
 		ArrayPole[1]=false;
+		if(pass2.value!=""){
+			asingpass()
+		}
 		anim();
 	}
 	else{
 		ArrayPole[1]=true;
+		pass.style.border="2px solid #49a35d";
+		if(pass2.value!=""){
+			asingpass()
+		}
 		anim();
-	}
-	if(pass2.value!=""){
-		onchangepass2();
 	}
 }
 
+function asingpass(){
+	var text = pass2.value;
+	var error="";
+	if(text!=pass.value){
+		pass2.style.border="2px solid red";
+		ArrayPole[2]=false;
+	}
+	else{
+		pass2.style.border="2px solid #49a35d";;
+		ArrayPole[2]=true;
+	}
+}
 
 function onchangepass2(){
 	var text = pass2.value;
@@ -63,21 +89,37 @@ function onchangepass2(){
 	}
 	if(error!=""){
 		alert(error);
+		pass2.style.border="2px solid red";
 		ArrayPole[2]=false;
 		anim();
 	}
 	else{
+		pass2.style.border="2px solid #49a35d";
 		ArrayPole[2]=true;
 		anim();
 	}
 }
-var ArrayPole=[false,false,false,false];
-var LeftPX=[0,125,250,375,500]
-var brr = document.getElementById("brick");
-var l=0;
-function an(){
-	brr.style.left="100px";
+
+function onchangeabout(){
+	var text = about.value;
+	var error="";
+	if(text.length<=10){
+		error+="Осебе должно быть больше 10-ти символов\n";
+	}
+	if(error!=""){
+		alert(error);
+		about.style.border="2px solid red";
+		ArrayPole[3]=false;
+		anim();
+	}
+	else{
+		about.style.border="2px solid #49a35d";
+		ArrayPole[3]=true;
+		anim();
+	}
 }
+
+
 function anim(){
 	var caunter=0;
     for(var i=0;i<ArrayPole.length;i++){
@@ -93,24 +135,28 @@ function anim(){
 			var T = setInterval(function() {
 			        if (l == LeftPX[caunter]) return clearInterval(T);
 			        l--;
+			        if(l%6==0){
+			        	p--;
+			        	brr.innerHTML=p+"%";
+			        }
 			        brr.style.left=l+"px";
 			    }, 5);
     	}else{
 		    		var TT = setInterval(function() {
 		        if (l == LeftPX[caunter]) return clearInterval(TT);
 		        l++;
+		        if(l%6==0){
+		        	p++;
+		        	brr.innerHTML=p+"%";
+			    }
 		        brr.style.left=l+"px";
 		    }, 5);
     	}
-
- // 40 мс.
-}
-    //frime(caunter);
-
+	}
 }
 
 
 function regis() {
-	alert(login.value)
+	alert(brr.innerHTML)
 }
 

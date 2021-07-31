b302c90c2b2cbd08996f19f35b745c1dbbe86e17
v.stasiuk <v.stasiuk@student.csn.khai.edu>
2021-08-01 00:46:09 +0300

    yl

--- README.md
+++
@@ -1,3 +0,0 @@
-Link static cache: https://static-cache-pwa.netlify.com
-
-Link dynamic cache: https://dynamic-cache-pwa.netlify.com
---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://vk.com/js/api/openapi.js?169" type="text/javascript"></script>
+</head>
+<body>
+
+<!--<div id="Yulka" style="display: flex;-->
+<!--    width: 100%;-->
+<!--    height: 100vh;-->
+<!--    justify-content: center;-->
+<!--    align-items: center;-->
+<!--    color: #e62626;-->
+<!--    font-size: 108px;-->
+<!--    font-weight: 700;"></div>-->
+
+<?php
+//    $res = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids=71406109&fields=relation,relation_partner&name_case=Nom&access_token=1da187289658017d8750a3af826c36b966f371506fa9bf207eb0bff778b20d54cc77831a26ebee1f920c8&v=5.131"),true);
+//    print_r($res);
+   // echo "@@@@";
+
+
+?>
+
+<!--<script type="text/javascript">-->
+<!--    var script = document.createElement('SCRIPT');-->
+<!--    script.src = "https://api.vk.com/method/users.get?user_ids=71406109&fields=relation,relation_partner&name_case=Nom&access_token=1da187289658017d8750a3af826c36b966f371506fa9bf207eb0bff778b20d54cc77831a26ebee1f920c8&v=5.131&callback=callbackFunc";-->
+<!--    document.getElementsByTagName("head")[0].appendChild(script);-->
+<!--    function callbackFunc(result) {-->
+<!--        if(result.response[0].relation){-->
+<!--            Yulka = document.getElementById("Yulka");-->
+<!--            Yulka.innerHTML = "Юлька обиделась:(";-->
+<!--        }-->
+<!--            console.log(result.response[0].relation);-->
+<!--    }-->
+<!--</script>-->
+
+</body>
+
+</html>

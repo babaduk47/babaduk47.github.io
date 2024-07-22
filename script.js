30f4dfd52137ab5b4cec43d4eb523d4dbe996070
v.stasiuk <vladbarsyk221999@gmail.com>
2024-07-23 00:03:48 +0400

    v1.1.12 - add map "2024-07-23"

--- script.js
+++ script.js
@@ -37,6 +37,22 @@ const maps = {
         ],
         key: {id: 12, x: 0, y: 2, width: 2, height: 1, direction: 'horizontal'},
         exitPosition: {x: 6, y: 2}
+    },
+    '2024-07-23': {
+        blocks: [
+            {id: 1, x: 0, y: 1, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 2, x: 1, y: 1, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 3, x: 2, y: 0, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 4, x: 4, y: 1, width: 1, height: 2, direction: 'vertical', color: 'red'},
+            {id: 5, x: 5, y: 1, width: 1, height: 3, direction: 'vertical', color: 'red'},
+            {id: 6, x: 3, y: 3, width: 1, height: 3, direction: 'vertical', color: 'red'},
+            {id: 7, x: 0, y: 0, width: 2, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 7, x: 3, y: 0, width: 2, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 8, x: 4, y: 4, width: 2, height: 1, direction: 'horizontal', color: 'green'},
+            {id: 9, x: 2, y: 2, width: 2, height: 1, direction: 'horizontal', color: 'blue'},
+        ],
+        key: {id: 9, x: 2, y: 2, width: 2, height: 1, direction: 'horizontal'},
+        exitPosition: {x: 6, y: 2}
     }
 };
 
@@ -268,7 +284,7 @@ function startCountdown(targetDate) {
     const interval = setInterval(updateCountdown, 1000);
 }
 
-const targetDate = new Date('Sun Jul 22 2024 20:30:00 GMT+0000').getTime();
+const targetDate = new Date('Sun Jul 23 2024 20:30:00 GMT+0000').getTime();
 const beforeFourHours = new Date(targetDate - 4 * 60 * 60 * 1000).getTime();
 const nowTime = new Date().getTime();
 
@@ -277,7 +293,7 @@ if (targetDate > nowTime && beforeFourHours < nowTime) {
 }
 
 const dateParam = getQueryParam('date');
-const date = '2024-07-22';
+const date = '2024-07-23';
 
 const dateDisplay = document.getElementById('date-display');
 dateDisplay.textContent = date;
